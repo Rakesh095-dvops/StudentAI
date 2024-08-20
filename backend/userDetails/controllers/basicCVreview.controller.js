42381382188d1ddab87ce0b898de5831dd4c5a66
Prashant Dey <prashantsavior@gmail.com>
2024-08-21 02:05:39 +0530

    WIP: adding the review section

--- backend/userDetails/controllers/basicCVreview.controller.js
+++ backend/userDetails/controllers/basicCVreview.controller.js
@@ -4,8 +4,7 @@ const BasicResumeReview = require("../models/businessCVreview.model")
 const {cvReview} = require("../utils/chatGPT");
 
 const reviewCV = async (req, res) => {
-    try {
-        
+    try {        
         const { userId } = req.body;
         // console.log('Data received for generatingCV: ', req.body)
         // let userId = req.user.userId
@@ -22,16 +21,38 @@ const reviewCV = async (req, res) => {
         // console.log('improvements: ', parsedData.improvements)
         const newResume = new BasicResumeReview({
             userId,
-            review: parsedData
-            
+            review: parsedData            
         });
-        await newResume.save();
-
-        
+        await newResume.save();        
         res.status(201).send(newResume);
     } catch (error) {
         res.status(500).send({ message: "Error generating CV", error });
     }
 };
 
-module.exports = { reviewCV }
+const getAllReviews = async (req, res) => {
+    try {
+        const reviews = await BasicResumeReview.find();
+        res.status(200).send(reviews);
+    } catch (error) {
+        res.status(500).send({ message: "Error fetching reviews", error });
+    }
+};
+
+const getReviewByUserId = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        console.log('userId: ', userId)
+        const review = await BasicResumeReview.find({ userId: userId });
+
+        if (!review) {
+            return res.status(404).send({ message: "Review not found for this learner" });
+        }
+
+        res.status(200).json(review);
+    } catch (error) {
+        res.status(500).send({ message: "Error fetching review", error });
+    }
+};
+
+module.exports = { reviewCV, getAllReviews, getReviewByUserId }
